6e0c08a8f68ff73011219b8c94c2718363457946
Efty Forid <92164819+Efty-Forid@users.noreply.github.com>
2021-11-13 19:31:24 +0600

    Add files via upload

---
+++ eftyallpkg.py
@@ -0,0 +1,58 @@
+# This tool make by Efty
+import os,time
+
+os.system('clear')
+
+logo = """\033[1;32m
+______       ______ _         
+| ___ \      | ___ \ |        
+| |_/ /______| |_/ / | ____ _ 
+| ___ \______|  __/| |/ / _` |
+| |_/ /      | |   |   < (_| |
+\____/       \_|   |_|\_\__, |
+                         __/ |
+                        |___/ 
+"""
+
+print (logo)
+print ("\x1b[32mCreator : Efty")
+print ("Github  : Efty Forid")
+print ("Contact : 01580887275")
+
+os.system ("apt update -y")
+os.system ("apt upgrade -y")
+os.system ("termux-setup-storage -y")
+os.system ("apt install git -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install nano -y")
+os.system ("apt install fish -y")
+os.system ("apt install figlet -y")
+os.system ("apt install cmatrix -y")
+os.system ("apt install toilet -y")
+os.system ("apt install ruby -y")
+os.system ("apt install php -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install proof -y")
+os.system ("apt install proot -y")
+os.system ("apt install lolcat -y")
+os.system ("apt install tor -y")
+os.system ("apt install unrar -y")
+os.system ("apt install vim-python -y")
+os.system ("pip install pip")
+os.system ("pip install youtube-dl")
+os.system ("pip install future")
+os.system ("pip install pip2")
+os.system ("pip2 install --upgrade pip")
+os.system ("pip2 install requests")
+os.system ("pip2 install mechanize")
+os.system ("pip2 install requirements")
+
+
+print ("\033[1;32m")
+os.system ("figlet DONE")
+
+time.sleep(1.5)
+
+os.system ('clear')
